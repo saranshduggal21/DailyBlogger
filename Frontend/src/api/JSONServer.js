cfcb532bd171b6e5b9add2a124679f968b6cb980
Saransh Duggal <46230124+saranshduggal21@users.noreply.github.com>
2021-09-26 09:37:00 -0400

    Delete Frontend/src/api directory

--- Frontend/src/api/JSONServer.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://b145-2620-101-f000-741-00-16c3.ngrok.io" 
-    //Now we've created an Axios instance that is pre-configured to talk to the baseURL. 
-    
-});
